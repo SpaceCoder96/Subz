22431ea98cb6a2999c5d565b4c706dc2b0d86bb6
Viraj Paradkar <virajsparadkar@gmail.com>
2023-02-06 18:19:01 +0530

    C1

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 from tkinter import *
+import sqlite3
+import configparser
+import matplotlib.pyplot as plt
+import math
+from fpdf import FPDF
+
+def add():
+    pass
+
+def rem():
+    pass
+
+def edit():
+    pass
+
+def config():
+    pass
+
+def summary():
+    pass
+
+def initiate():
+    pass
 
 root = Tk()
 root.title("Subz")
 root.geometry("720x480")
+
+
 
 root.mainloop()
